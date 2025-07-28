0814000de442839be2472cec46c5db672e40c8c9
Surendar Raj <gsurend4@ford.com>
2025-07-28 18:42:48 +0530

    Added SQLite Data Base

--- flask_API/app.py
+++ flask_API/app.py
@@ -1,124 +1,159 @@
 from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
 print("Hello")
 
+# SQL lite DB Connection.
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("books.sqlite")
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
 # In-memory DB/Object
-books_list=[
-     {
-         'id': 0,
-         "author":"chinua achebe",
-         "language":"english",
-         "title":"things fall apart",
-     },
-     {
-         'id': 1,
-         "author": "hans christian andersen",
-         "language": "danish",
-         "title": "fairy tales",
-     },
-     {
-         'id': 2,
-         "author": "samuel beckett",
-         "language": "french,english",
-         "title": "molloy,malone dies,the unnamable,the triology",
-     },
-     {
-         'id': 6,
-         "author": "jorge luis borges",
-         "language": "spanish",
-         "title": "ficciones",
-     },
-     {
-         'id': 3,
-         "author": "giovanni boccaccio",
-         "language": "italian",
-         "title": "the decameron",
-     },
-     {
-         'id': 5,
-         "author": "emily bront",
-         "language": "english",
-         "title": "wuthering heights",
-     },
- ]
+# books_list=[
+#      {
+#          'id': 0,
+#          "author":"chinua achebe",
+#          "language":"english",
+#          "title":"things fall apart",
+#      },
+#      {
+#          'id': 1,
+#          "author": "hans christian andersen",
+#          "language": "danish",
+#          "title": "fairy tales",
+#      },
+#      {
+#          'id': 2,
+#          "author": "samuel beckett",
+#          "language": "french,english",
+#          "title": "molloy,malone dies,the unnamable,the triology",
+#      },
+#      {
+#          'id': 6,
+#          "author": "jorge luis borges",
+#          "language": "spanish",
+#          "title": "ficciones",
+#      },
+#      {
+#          'id': 3,
+#          "author": "giovanni boccaccio",
+#          "language": "italian",
+#          "title": "the decameron",
+#      },
+#      {
+#          'id': 5,
+#          "author": "emily bront",
+#          "language": "english",
+#          "title": "wuthering heights",
+#      },
+#  ]
 
 # CRUD Operations
 
 @app.route('/books', methods = ['GET', 'POST'])
 def books():
+
+    conn = db_connection()
+
+    cursor = conn.cursor()
+
     if request.method == 'GET':
+        sql = """ select * from book """
+        cursor = cursor.execute(sql)
+
+        #Converting the cursor object to a Dictionary.
+        # fetchall will return list of tuples where each tuple will represent a row in DB
+        books_list = [ { 'id' : row[0], 'author' : row[1], 'language' : row[2], 'title' : row[3] } for row in cursor.fetchall() ]
+
         if len(books_list) > 0:
             return jsonify(books_list), 200
-        else:
-            return 'Nothing found', 404
+        
     
     if request.method == 'POST':
         new_author = request.form['author']
         new_language = request.form['language']
         new_title = request.form['title']
 
-        new_id = books_list[-1]['id'] + 1
+        sql = """ Insert into book (author, language, title) 
+                  values(?, ?, ?)"""
+        
+        cursor = cursor.execute(sql, (new_author, new_language, new_title, ))
 
-        new_books_list = {
-            'id' : new_id,
-            'author' : new_author,
-            'language' : new_language,
-            'title' : new_title 
-        }
+        conn.commit() # Helps you to commit the changes to the DB.
 
-        books_list.append(new_books_list)
-
-        return jsonify(books_list), 201
+        return f"The Book has been added to the DB under the ID {cursor.lastrowid} ", 201
     
-    else:
-        return 'Nothing Found', 404
-    
+
+    return "Sorry We did not find the DB", 404
+
+
 @app.route('/books/<int:id>', methods = ['GET', 'PUT', 'DELETE'])
 def single_book(id):
+
+    conn = db_connection()
+
+    cursor = conn.cursor()
+
     if request.method == 'GET':
-        for book in books_list:
-            if book['id'] == id:
-                return jsonify(book), 200
-        
-        return 'No such Book ID present', 404
+
+        sql = """ select * from book where id = ? """
+
+        cursor = cursor.execute(sql, (id,))
+
+        requested_book = [ dict(id = row[0], author = row[1], language = row[2], title = row[3]) for row in cursor.fetchall() ]
+
+        if len(requested_book) == 1:
+            return jsonify(requested_book), 200
+        else:
+            return f"The requested book ID {id} is not present in the book DB"
     
     if request.method == 'PUT':
-        for book in books_list:
-            if book['id'] == id:
-                book['title'] = request.form['title']
-                book['language'] = request.form['language']
-                book['author'] = request.form['author']
+        
+        title = request.form['title']
+        language = request.form['language']
+        author = request.form['author']
 
-                updated_book = {
-                    'id' : book['id'],
-                    'title' : book.title,
-                    'language' : book.language,
-                    'author' : book.author
-                }
+        sql = """ update book 
+                  set title = ?,
+                      language = ?,
+                      author = ?
+                  where id = ?
+              """
+        
+        cursor = cursor.execute(sql, (title, language, author, id))
 
-                return jsonify(updated_book), 201
-            
-        else:
-            new_book = {
-                'id' : id,
-                'title' : request.form['title'],
-                'language' : request.form['language'],
-                'author' : request.form['author']
-            }
+        cursor1 = cursor.execute("select * from book")
 
-            books_list.append(new_book)
+        book_list = [ dict(id = row[0], author = row[1], language = row[2], title = row[3]) for row in cursor1.fetchall() ]
 
-            return jsonify(books_list), 200
+        conn.commit()
 
+        return jsonify(book_list), 200
     
-    if request.method == 'DELETE':
-        for index, book in enumerate(books_list):
-            if book['id'] == id:
-                books_list.pop(index)
 
-                return jsonify(books_list), 200
+
+    if request.method == 'DELETE':
+        
+        sql = """ delete from book where id = ? """
+
+        cursor.execute(sql, (id,))
+
+        conn.commit()
+
+        cursor1 = cursor.execute("select * from book")
+
+        deleted_book = [ dict(id = row[0], author = row[1], language = row[2], title = row[3]) for row in cursor1.fetchall() ]
+
+        return jsonify(deleted_book), 200
+    
+    return "Sorry We did not find the DB", 404
 
             
         
